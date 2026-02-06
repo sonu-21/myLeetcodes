1aeb0d70bb45570b69899e04dd3509b3e29681f1
sonu beldar <s00nubl86@gmail.com>
2026-02-06 09:53:19 +0530

    Time: 60 ms (21.8%), Space: 90.6 MB (69.18%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+         unordered_map<int,int>mpp;
+         for(int i = 0 ;i<nums.size() ;i++) {
+             if(mpp[nums[i]] >= 1) return true;
+
+             mpp[nums[i]]++;
+         }
+         return false;
+
+        
+
+       
+    }
+};
