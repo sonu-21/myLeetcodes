44d0bfeeac3619fb8365b2bc24c66de09c2ee82c
sonu beldar <s00nubl86@gmail.com>
2026-02-10 18:53:52 +0530

    Time: 0 ms (100%), Space: 18.7 MB (23.12%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -63,9 +63,9 @@ public:
         for(int i = 0 ;i<m ; i++){
             for(int j = 0 ; j<n ; j++){
                 if(matrix[i][j] == 0){
-                    matrix[0][j]=0;
+                    matrix[0][j] = 0;
                 if(i>0){
-                    matrix[i][0]=0;
+                    matrix[i][0] = 0;
                 }
                 else{
                     rowZero = true;
@@ -75,19 +75,26 @@ public:
         }
         for(int i =  1;i<m; i++ ){
             for(int j = 1;j<n;j++){
-                if(matrix[0][j] == 0 || matrix[i][0]){
+                if(matrix[0][j] == 0 || matrix[i][0]==0){
                     matrix[i][j] = 0 ;
                 }
             }
         }
-        if(rowZero){
-            for(int i = 0 ; i<m ; i++){
-                matrix[0][i]=0;
+         if(matrix[0][0]==0){
+            for(int i= 0 ;i<m ;i++){
+                matrix[i][0]=0;
             }
         }
+        if(rowZero == true){
+            for(int j = 0 ; j<n ; j++){
+                matrix[0][j]=0;
+            }
+        }
+       
         
     }
     void setZeroes(vector<vector<int>>& matrix) {
-       better(matrix);
+    //    better(matrix);
+        optimal(matrix);
     }
 };
