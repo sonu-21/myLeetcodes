3bfeff6e9554cfc46c0c630622a2291dea640e88
sonu eldar <s00nubl86@gmail.com>
2026-02-26 22:16:51 +0530

    Time: 0 ms (100%), Space: 8.3 MB (36.39%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if(s.size()!=goal.size()) return false;
+        s = s+s;
+
+        if(s.contains(goal)) return true;
+        else return false;
+    }
+};
