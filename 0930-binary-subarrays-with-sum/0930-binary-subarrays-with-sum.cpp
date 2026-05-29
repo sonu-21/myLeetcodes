245895d9ff4e29c391f108beae36a5a7f8d87d41
Sonu Beldar <s00nubl86@gmail.com>
2026-05-30 01:38:54 +0530

    Time: 0 ms (100%), Space: 32.5 MB (98.47%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -1,18 +1,29 @@
 class Solution {
 public:
     int numSubarraysWithSum(vector<int>& nums, int goal) {
-       int cnt = 0 ;
-     unordered_map<int,int>freq;
-     freq[0] = 1;
-    int preSum = 0;
-     for( auto x : nums){
-        preSum += x;
+        int result = 0;
+        int l = 0, r = 0;
+        int window_sum = 0;
+        int cntzero = 0;
 
-        if(freq.find(preSum-goal)!=freq.end()){
-            cnt = cnt + freq[preSum-goal];
+        while( r < nums.size()){
+            window_sum += nums[r];
+
+            while( l<r && (nums[l] == 0 ||window_sum > goal)){
+                if(nums[l] == 0){
+                    cntzero++;
+                }
+                else{
+                    cntzero = 0;
+                }
+                window_sum -= nums[l];
+                l++;
+            }
+            if(window_sum == goal){
+                result += 1+cntzero;
+            }
+            r++;
         }
-        freq[preSum]++;
-     } 
-     return cnt;  
+        return result;
     }
 };
