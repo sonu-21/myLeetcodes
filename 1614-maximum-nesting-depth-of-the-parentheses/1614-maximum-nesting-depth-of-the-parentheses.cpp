be380cb610898ee0bda52eb6e8330420b11e0a92
Sonu Beldar <s00nubl86@gmail.com>
2026-08-27 11:50:46 +0530

    Time: 0 ms (100%), Space: 8.3 MB (61.05%) - LeetHub

--- 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     int maxDepth(string s) {
-        int ans = 0 ;
-        int cnt  = 0 ; 
-        for( auto  x : s){
-            if( x == '('){
+        int depth = 0 ;
+        int cnt = 0;
+        for(auto x : s) {
+            if(x == '(') {
                 cnt++;
-                ans = max(ans,cnt);
+                depth = max(cnt,depth);
             }
-            else if(x == ')'){
-                cnt--;
-            }
+            else if(x==')') cnt--;
+
         }
-        return ans;
+        return depth;
     }
 };
